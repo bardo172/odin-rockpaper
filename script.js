4f4bf74338b91c30a879fa380394ffdf45c61959
Bardo <bafe172@gmail.com>
2024-10-24 10:53:31 -0600

    Initial commit
    Add empty index.html and script.js files

---
+++ script.js
@@ -0,0 +1,12 @@
+// Print a message to confirm the script is working
+console.log("script.js is linked and working!");
+
+// Basic example function
+function greetUser() {
+    alert("Welcome to the website!");
+}
+
+// Call the function when the page loads
+window.onload = function() {
+    greetUser();
+};
